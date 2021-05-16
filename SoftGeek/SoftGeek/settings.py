88ca5f6908f16060df0eed6fb266f36822bad265
Enock Hamuli <58560356+enockhamuli15@users.noreply.github.com>
2021-05-16 11:58:12 +0200

    Update settings.py

--- SoftGeek/SoftGeek/settings.py
+++ SoftGeek/SoftGeek/settings.py
@@ -141,4 +141,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'enockhamuli@gmail.com'
-EMAIL_HOST_PASSWORD = '10017515'
+EMAIL_HOST_PASSWORD = '*******'
